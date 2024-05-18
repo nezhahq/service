9ae0db11e8df1582ad1bc543ff84eb8a5b527d77
UUBulb <35923940+uubulb@users.noreply.github.com>
2024-05-18 12:37:36 +0800

    Module rename & change Control() behavior, Add WorkingDirectory support for OpenRC (#1)

--- example/logging/main.go
+++ example/logging/main.go
@@ -10,7 +10,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/kardianos/service"
+	"github.com/nezhahq/service"
 )
 
 var logger service.Logger
--- service_su_test.go
+++ service_su_test.go
@@ -18,7 +18,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kardianos/service"
+	"github.com/nezhahq/service"
 )
 
 const runAsServiceArg = "RunThisAsService"
